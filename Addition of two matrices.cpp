2ef7806b1f0cd59f818a3348534f37c6ccb987cc
65NinadA3 <ninadphatak123@gmail.com>
2025-08-10 16:26:50 +0530

    Create Addition of two matrices.cpp

---
+++ Addition of two matrices.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int rows, cols;
+    cout << "Enter number of rows: ";
+    cin >> rows;
+    cout << "Enter number of columns: ";
+    cin >> cols;
+int A[100][100], B[100][100], sum[100][100];
+cout << "\nEnter elements of first matrix:\n";
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            cin >> A[i][j];
+        }
+    }
+cout << "\nEnter elements of second matrix:\n";
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            cin >> B[i][j];
+        }
+    }
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            sum[i][j] = A[i][j] + B[i][j];
+        }
+    }
+    cout << "\nSum of matrices:\n";
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            cout << sum[i][j] << " ";
+        }
+        cout << endl;
+    }
+  return 0;
+}
